aec43c300912b2ccfb1055c5128f6ebd9258efcf
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-08 18:07:44 +0300

    ft:adding_comments_model:backend

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -10,3 +10,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text
+
+class Comment(models.Model):
+    tweet = models.ForeignKey(Post,on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tweet.text
