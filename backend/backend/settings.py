0f4c8aa3de95d8520bd7b99c025f19a007be6eab
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-08 17:31:24 +0300

    ft:adding_moddelling_backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweets.apps.TweetsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
