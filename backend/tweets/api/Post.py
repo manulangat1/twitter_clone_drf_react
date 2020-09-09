c86382c7717411728d44ed666a5907b6a09a46fc
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-09 21:46:31 +0300

    ft:adding_user_auth_apis:backend

--- backend/tweets/api/Post.py
+++ backend/tweets/api/Post.py
@@ -3,12 +3,53 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+from knox.models import AuthToken
+
 from ..serializers.Post import (
     PostSerializer, CommentSerializer,
     PostDetailSerializer, LikeSerializer, 
-    TagSerializer, TagDetailSerializer )
+    TagSerializer, TagDetailSerializer , 
+    RegisterSerializer, UserSerializer ,
+    LoginSerializer
+    )
 from ..models import Post,Comment,Like,Tags
 
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class =LoginSerializer
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data
+        })
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+    def get_object(self):
+        return self.request.user
 
 class TagCreate(generics.CreateAPIView):
     serializer_class = TagSerializer
