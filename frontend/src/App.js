dcf7a76a3be4489bf3228b5a2fe36b4ca3cb2c58
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-15 18:02:40 +0300

    ft:configuring_react_router_dom:frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider} from 'react-redux'
+import { HashRouter as Router,Switch,Route} from 'react-router-dom'
 import './App.css';
 
 import store from './store'
@@ -8,9 +9,14 @@ class App extends React.Component{
   render(){
     return(
       <Provider store={store}>
+        <Router>
       <section>
+        <Switch>
         <h1>hey</h1>
+        </Switch>
+        
       </section>
+      </Router>
       </Provider>
     )
   }
