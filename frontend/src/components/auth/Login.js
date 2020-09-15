e3953dfcba1225c852bf41b1603ed7ac70f438fe
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-15 19:15:58 +0300

    ft:adding_LoginComponent:frontend

---
+++ frontend/src/components/auth/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink,withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
+
+class Login extends React.Component{
+    render(){
+        return(
+            <section>
+
+            </section>
+        )
+    }
+}
+export default Login
