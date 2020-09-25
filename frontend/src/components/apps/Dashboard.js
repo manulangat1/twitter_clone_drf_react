0bda84f73dd4e08b839fb5b5ca63549405e149ba
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-25 20:06:41 +0300

    ft-adding-functionality

--- frontend/src/components/apps/Dashboard.js
+++ frontend/src/components/apps/Dashboard.js
@@ -5,10 +5,20 @@ import AddPost from './AddPost'
 class Dashboard extends React.Component{
     render(){
         return(
-            <section>
+            <section className="dashboard">
                 <div className="container">
-                <AddPost />
-                <LoadPost />
+                <div className="grid">
+                    <div className="mains">
+                        <h1>Hey there</h1>
+                    </div>
+                    <div>
+                    <AddPost />
+                    <LoadPost />
+                    </div>
+                    <div className="trending">
+                        <h1>Nop</h1>
+                    </div>
+                </div>
                 </div>
             </section>
         )
